c9eac7306e1fde3e90136f9b309872fb08f150cd
Paz Aricha <paz@pazaricha.com>
2014-06-24 13:12:48 +0300

    lo itfos

--- lib/elastic_twitter.rb
+++ lib/elastic_twitter.rb
@@ -1,2 +1,17 @@
 class ElasticTwitter
+  def self.connection
+    @connection ||= Elasticsearch::Client.new
+  end
+
+  def self.index_name
+    "lo_itfos"
+  end
+
+  def initialize(query)
+    @query = query
+  end
+
+  def search
+    self.class.connection.search index: self.class.index_name, body: { query: @query }
+  end
 end
